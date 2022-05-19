cd5ddf7649f0116ef573632ea805fb6a3185795e
nickguerra <nickg@maliciousgames.com>
2022-05-19 05:02:35 -0500

    Added helper methods

---
+++ SuggestionAppUI/Helpers/AuthenticationStateProviderHelpers.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuggestionAppUI.Helpers;
+public static class AuthenticationStateProviderHelpers
+{
+    public static async Task<UserModel> GetUserFromAuth(this AuthenticationStateProvider provider, IUserData userData)
+    {
+        var authState = await provider.GetAuthenticationStateAsync();
+        string objectId = authState.User.Claims.FirstOrDefault(c => c.Type.Contains("objectidentifier"))?.Value;
+        return await userData.GetUserFromAuthentication(objectId);
+    }
+}
